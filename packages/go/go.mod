5da2e5159071235d997eca2ba52dd9e4f3be5101
hasirciogli <mhasirciogli@gmail.com>
2025-01-23 23:32:01 +0300

    frst test release

---
+++ packages/go/go.mod
@@ -0,0 +1,3 @@
+module github.com/hasirciogli/x-protocol/packages/go
+
+go 1.23.4
---
+++ packages/go/index.go
@@ -0,0 +1,11 @@
+package xprotocol
+
+import "fmt"
+
+func HelloWorld() string {
+	return "Hello, world!"
+}
+
+func PrintMessage(msg string) {
+	fmt.Println(msg)
+}
