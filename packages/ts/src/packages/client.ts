c0cfef1f85a56609eb2cdae2d3fa61830d4af85e
hasirciogli <mhasirciogli@gmail.com>
2025-02-24 20:45:06 +0300

    cliend added for production use

--- packages/ts/src/packages/client.ts
+++ packages/ts/src/packages/client.ts
@@ -1,19 +1,78 @@
 // TypeScript Client Implementation
 
-export class Client {
-    private serverUrl: string;
+export interface XProtocolClientCallResponse {
+  success: boolean;
+  data: any; // json.RawMessage
+  error?: string;
+}
 
-    constructor(serverUrl: string) {
-        this.serverUrl = serverUrl;
+export interface XProtocolClientCallRequest {
+  proxyChannelName?: string; // proxy channel name for proxy mode xprotocol server
+  name: string; // function name
+  payload: any; // json.RawMessage
+}
+
+export class XProtocolClient {
+  private host: string;
+  private port: number;
+
+  constructor(host: string, port: number) {
+    this.host = host;
+    this.port = port;
+  }
+
+  public async call(
+    request: XProtocolClientCallRequest
+  ): Promise<XProtocolClientCallResponse> {
+    const bodyTextJson = JSON.stringify(request);
+
+    if (process.env.APP_MODE === "development") {
+      console.log(`Call isteği gönderildi -> ${request.name}`);
     }
 
-    public connect() {
-        console.log(`Connecting to server at ${this.serverUrl}...`);
-        // Burada bağlantı mantığını ekleyebilirsiniz.
-    }
+    try {
+      const response = await fetch(`http://${this.host}:${this.port}/calls`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: bodyTextJson,
+      });
 
-    public sendRequest(data: any) {
-        console.log(`Sending request with data: ${JSON.stringify(data)}`);
-        // Burada istek gönderme mantığını ekleyebilirsiniz.
+      const responseBody = await response.json();
+
+      if (!response.ok) {
+        return {
+          success: false,
+          data: null,
+          error: responseBody.error || "Unknown error",
+        };
+      }
+
+      if (process.env.APP_MODE === "development") {
+        console.log(
+          `Call yanıtı alındı -> ${JSON.stringify(responseBody.data)}`
+        );
+      }
+
+      return {
+        success: true,
+        data: responseBody.data,
+        error: responseBody.error,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        data: null,
+        error: error instanceof Error ? error.message : "Unknown error",
+      };
     }
-} 
+  }
+}
+
+export function newXProtocolClient(
+  host: string,
+  port: number
+): XProtocolClient {
+  return new XProtocolClient(host, port);
+}
